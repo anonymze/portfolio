35fc7c70158104e9cba5e7a6934660a4461a94d9
Yann <yann.metier@inconito.fr>
2020-10-05 12:02:59 +0200

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,65 @@
 import Layout from "../components/Layout";
-import home_module from '../styles/pages/home.module.css';
+import home_module from "../styles/pages/home.module.css";
 import { motion } from "framer-motion";
-import Contact from '../components/Contact';
+import Contact from "../components/Contact";
 
 const variants = {
   hide: {
     x: -1500,
-    opacity: 0
+    opacity: 0,
   },
   show: {
     x: 0,
-    opacity: 1
-  }
-}
+    opacity: 1,
+  },
+};
 
 export default function Home() {
-  
   return (
-    <Layout home>     
+    <Layout home>
       <motion.div
         variants={variants}
         initial="hide"
         animate="show"
         transition={{ ease: "anticipate", duration: "0.8" }}
-        className={home_module.presentation}>
+        className={home_module.presentation}
+      >
         <div className={home_module.presentation_left}>
           <p className={home_module.left_title}>Bonjour, je suis Yann,</p>
-          <p className={home_module.left_text}>un développeur web/mobile full stack</p>
+          <p className={home_module.left_text}>
+            un développeur web/mobile full stack
+          </p>
           <div>
-          <Contact />
+            <Contact />
           </div>
         </div>
         <div className={home_module.presentation_right}>
-          <p className={home_module.right_text}>Je suis un développeur basé à Toulouse. Je travail pour la compagnie <span className={home_module.langages}>Inconito</span> en tant qu'alternant, je suis sur ma première année d'experience avec le digital.<br />
-          J'ai une expérience junior avec l'<span className={home_module.langages}>HTML</span>, le <span className={home_module.langages}>JS</span> et le <span className={home_module.langages}>CSS</span>, et j'utilise régulièrement les frameworks <span className={home_module.langages}>React</span>, <span className={home_module.langages}>NextJs</span>, <span className={home_module.langages}>Laravel</span>, <span className={home_module.langages}>Ionic</span>, <span className={home_module.langages}>Drupal</span>.
+          <p className={home_module.right_text}>
+            Je suis un développeur basé à Toulouse. Je travail pour la compagnie{" "}
+            <span className={home_module.langages}>Inconito</span> en tant
+            qu'alternant, je suis sur ma deuxième année d'experience avec le
+            digital.
+            <br />
+            J'ai une expérience junior avec l'
+            <span className={home_module.langages}>HTML</span>, le{" "}
+            <span className={home_module.langages}>JS</span> et le{" "}
+            <span className={home_module.langages}>CSS</span>, et je me
+            familiarise avec les frameworks{" "}
+            <span className={home_module.langages}>React</span>,{" "}
+            <span className={home_module.langages}>NextJs</span>,{" "}
+            <span className={home_module.langages}>Laravel</span>,{" "}
+            <span className={home_module.langages}>Ionic</span>,{" "}
+            <span className={home_module.langages}>Drupal</span>.
           </p>
-          <p className={home_module.right_text}>Je vise à accumuler de l'expérience <span className={home_module.langages}>full-stack</span> pour la polyvalence et l'autonomie.</p>
-          <p className={home_module.right_text}>Je fais énormément d'intégration mais je recherche un bagage plus important en back-end.</p>
+          <p className={home_module.right_text}>
+            Je vise à accumuler de l'expérience{" "}
+            <span className={home_module.langages}>full-stack</span> pour la
+            polyvalence et l'autonomie.
+          </p>
+          <p className={home_module.right_text}>
+            Je fais énormément d'intégration mais je recherche un bagage plus
+            important en back-end.
+          </p>
         </div>
       </motion.div>
       <article className="border-t-2 border-solid border-gray-300 border-b-2">
@@ -48,40 +71,95 @@ export default function Home() {
             initial="hide"
             animate="show"
             transition={{ ease: "anticipate", duration: "0.8" }}
-            className="flex flex-wrap justify-center w-100 ">
-
-            <div className="card relative flex m-10 w-2/5 bg-purple-600 rounded-lg transform transition-transform duration-200 ease-in-out hover:scale-105" style={{ minWidth: "800px", height: "500px", boxShadow: '0 4px 14px 0 var(--button-purple)' }}>
-              <div className="w-1/2 p-16">
+            className="flex flex-wrap justify-center w-100 "
+          >
+            <div
+              className="card relative flex m-10 w-2/5 bg-purple-600 rounded-lg transform transition-transform duration-200 ease-in-out hover:scale-105"
+              style={{
+                minWidth: "730px",
+                height: "450px",
+                boxShadow: "0 4px 14px 0 var(--button-purple)",
+              }}
+            >
+              <div className="w-1/2 p-12 m-1">
                 <p className="text-xl text-white">2020</p>
-                <h3 className="text-5xl font-bold text-white mt-2 mb-16">Association gaming T5</h3>
-                <p className="text-xl text-white leading-8">Full-stack développement
+                <h3
+                  className="text-5xl font-bold text-white mt-2 mb-16"
+                  style={{ lineHeight: "3rem" }}
+                >
+                  Association gaming T5
+                </h3>
+                <p className="text-xl text-white leading-8">
+                  Full-stack développement
                   <br />
-                Application web
+                  Application web
                 </p>
                 <div className="absolute" style={{ bottom: "4rem" }}>
-                  <img className="inline-block w-12 mx-2" src="/images/icones_langage/wordpress.png" alt="Logo wordpress" title="Wordpress" />
+                  <img
+                    className="inline-block w-12 mx-2 object-cover object-left"
+                    src="/images/icones_langage/wordpress.png"
+                    alt="Logo wordpress"
+                    title="Wordpress"
+                  />
                 </div>
               </div>
               <div className="w-1/2 py-8">
-                <img className="block h-full rounded-l-md" alt="" src="/images/images_card/t5.png" />
-              </div></div>
+                <img
+                  className="block w-full h-full rounded-l-md object-cover object-left"
+                  alt=""
+                  src="/images/images_card/t5.png"
+                />
+              </div>
+            </div>
 
-            <a className="card relative flex m-10 w-2/5 bg-blue-600 rounded-lg transform transition-transform duration-200 ease-in-out hover:scale-105" style={{ minWidth: "800px", height: "500px", boxShadow: '0 4px 14px 0 var(--button-blue)' }}>
-              <div className="w-1/2 p-16">
+            <a
+              className="card relative flex m-10 w-2/5 bg-blue-600 rounded-lg transform transition-transform duration-200 ease-in-out hover:scale-105"
+              style={{
+                minWidth: "730px",
+                height: "450px",
+                boxShadow: "0 4px 14px 0 var(--button-blue)",
+              }}
+            >
+              <div className="w-1/2 p-12 m-1">
                 <p className="text-xl text-white">2020</p>
-                <h3 className="text-5xl font-bold text-white mt-2 mb-16">Smash Lab</h3>
-                <p className="text-xl text-white leading-8">Full-stack développement
+                <h3
+                  className="text-5xl font-bold text-white mt-2 mb-16"
+                  style={{ lineHeight: "3rem" }}
+                >
+                  Smash Lab
+                </h3>
+                <p className="text-xl text-white leading-8">
+                  Full-stack développement
                   <br />
-                Application mobile
+                  Application mobile
                 </p>
                 <div className="absolute" style={{ bottom: "4rem" }}>
-                  <img className="inline-block w-12 mx-2" src="/images/icones_langage/ionic.png" alt="Logo Ionic" title="Ionic" />
-                  <img className="inline-block w-12 mx-2" src="/images/icones_langage/typescript.png" alt="Logo Typescript" title="Typescript" />
-                  <img className="inline-block w-12 mx-2" src="/images/icones_langage/laravel.png" alt="Logo Laravel" title="Laravel" />
+                  <img
+                    className="inline-block w-12 mx-2"
+                    src="/images/icones_langage/ionic.png"
+                    alt="Logo Ionic"
+                    title="Ionic"
+                  />
+                  <img
+                    className="inline-block w-12 mx-2"
+                    src="/images/icones_langage/typescript.png"
+                    alt="Logo Typescript"
+                    title="Typescript"
+                  />
+                  <img
+                    className="inline-block w-12 mx-2"
+                    src="/images/icones_langage/laravel.png"
+                    alt="Logo Laravel"
+                    title="Laravel"
+                  />
                 </div>
               </div>
               <div className="w-1/2 py-8">
-                <img className="block w-full h-full rounded-l-md object-contain object-left" alt="" src="/images/images_card/smash_lab.png" />
+                <img
+                  className="block w-full h-full rounded-l-md object-cover object-left"
+                  alt=""
+                  src="/images/images_card/smash_lab.png"
+                />
               </div>
             </a>
           </motion.div>
@@ -89,21 +167,70 @@ export default function Home() {
       </article>
       <article>
         <section className="m-16">
-          <h2 className="font-bold text-4xl mb-24">Expériences professionnelles</h2>
+          <h2 className="font-bold text-4xl mb-24">
+            Expériences professionnelles
+          </h2>
           <motion.div
             variants={variants}
             initial="hide"
             animate="show"
             transition={{ ease: "anticipate", duration: "0.8" }}
-            className="my-12 flex sm:flex-col justify-center items-center">
+            className="my-12 flex justify-center items-center"
+          >
             <div className="mx-16">
-              <img className="block w-24" src="/images/compagnies/inconito.png" aria-label="Logo de web6" />
+              <img
+                className="block w-24"
+                src="/images/compagnies/inconito.png"
+                aria-label="Logo de web6"
+              />
             </div>
             <div className="mx-16">
               <ul className="list-none">
-                <li className="w-auto text-white font-semibold text-xl h-8" style={{ margin: "0.8rem 0", width: "600px" }}><p style={{ backgroundColor: "#014d6f", lineHeight: "2.5rem", paddingRight: "1.1rem" }} className="inline-block rounded-md pl-4" >2019 / 2020 (1 an)</p></li>
-                <li className="w-auto text-white font-semibold text-xl h-8" style={{ margin: "0.8rem 0", width: "600px" }}><p style={{ backgroundColor: "#014d6f", lineHeight: "2.5rem", paddingRight: "1.1rem" }} className="inline-block rounded-md pl-4">Front-end</p></li>
-                <li className="w-auto text-white font-semibold text-xl h-8" style={{ margin: "0.8rem 0", width: "600px" }} ><p style={{ backgroundColor: "#014d6f", lineHeight: "2.5rem", paddingRight: "1.1rem" }} className="inline-block rounded-md pl-4">Intégrations de maquette et newsletters (mailchamp)</p></li>
+                <li
+                  className="w-auto text-white font-semibold text-xl h-8"
+                  style={{ margin: "0.8rem 0", width: "600px" }}
+                >
+                  <p
+                    style={{
+                      backgroundColor: "#014d6f",
+                      lineHeight: "2.5rem",
+                      paddingRight: "1.1rem",
+                    }}
+                    className="inline-block rounded-md pl-4"
+                  >
+                    2019 / 2020 (1 an)
+                  </p>
+                </li>
+                <li
+                  className="w-auto text-white font-semibold text-xl h-8"
+                  style={{ margin: "0.8rem 0", width: "600px" }}
+                >
+                  <p
+                    style={{
+                      backgroundColor: "#014d6f",
+                      lineHeight: "2.5rem",
+                      paddingRight: "1.1rem",
+                    }}
+                    className="inline-block rounded-md pl-4"
+                  >
+                    Front-end
+                  </p>
+                </li>
+                <li
+                  className="w-auto text-white font-semibold text-xl h-8"
+                  style={{ margin: "0.8rem 0", width: "600px" }}
+                >
+                  <p
+                    style={{
+                      backgroundColor: "#014d6f",
+                      lineHeight: "2.5rem",
+                      paddingRight: "1.1rem",
+                    }}
+                    className="inline-block rounded-md pl-4"
+                  >
+                    Intégrations de maquette et newsletters (mailchamp)
+                  </p>
+                </li>
               </ul>
             </div>
           </motion.div>
@@ -112,22 +239,67 @@ export default function Home() {
             initial="hide"
             animate="show"
             transition={{ ease: "anticipate", duration: "0.8" }}
-            className="my-12 flex sm:flex-col justify-center items-center">
+            className="my-12 flex justify-center items-center"
+          >
             <div className="mx-16">
-              <img className="block w-24" src="/images/compagnies/web6.png" aria-label="Logo de web6" />
+              <img
+                className="block w-24"
+                src="/images/compagnies/web6.png"
+                aria-label="Logo de web6"
+              />
             </div>
             <div className="mx-16">
               <ul className="list-none">
-                <li className="w-auto text-white font-semibold text-xl h-8" style={{ margin: "0.8rem 0", width: "600px" }}><p style={{ backgroundColor: "#00ccff", lineHeight: "2.5rem", paddingRight: "1.1rem" }} className="inline-block rounded-md pl-4" >2018 / 2019 (4 mois)</p></li>
-                <li className="w-auto text-white font-semibold text-xl h-8" style={{ margin: "0.8rem 0", width: "600px" }}><p style={{ backgroundColor: "#00ccff", lineHeight: "2.5rem", paddingRight: "1.1rem" }} className="inline-block rounded-md pl-4">Full-stack</p></li>
-                <li className="w-auto text-white font-semibold text-xl h-8" style={{ margin: "0.8rem 0", width: "600px" }}><p style={{ backgroundColor: "#00ccff", lineHeight: "2.5rem", paddingRight: "1.1rem" }} className="inline-block rounded-md pl-4">Création d'une application web de centralisation de stocks</p></li>
+                <li
+                  className="w-auto text-white font-semibold text-xl h-8"
+                  style={{ margin: "0.8rem 0", width: "600px" }}
+                >
+                  <p
+                    style={{
+                      backgroundColor: "#00ccff",
+                      lineHeight: "2.5rem",
+                      paddingRight: "1.1rem",
+                    }}
+                    className="inline-block rounded-md pl-4"
+                  >
+                    2018 / 2019 (4 mois)
+                  </p>
+                </li>
+                <li
+                  className="w-auto text-white font-semibold text-xl h-8"
+                  style={{ margin: "0.8rem 0", width: "600px" }}
+                >
+                  <p
+                    style={{
+                      backgroundColor: "#00ccff",
+                      lineHeight: "2.5rem",
+                      paddingRight: "1.1rem",
+                    }}
+                    className="inline-block rounded-md pl-4"
+                  >
+                    Full-stack
+                  </p>
+                </li>
+                <li
+                  className="w-auto text-white font-semibold text-xl h-8"
+                  style={{ margin: "0.8rem 0", width: "600px" }}
+                >
+                  <p
+                    style={{
+                      backgroundColor: "#00ccff",
+                      lineHeight: "2.5rem",
+                      paddingRight: "1.1rem",
+                    }}
+                    className="inline-block rounded-md pl-4"
+                  >
+                    Création d'une application web de centralisation de stocks
+                  </p>
+                </li>
               </ul>
             </div>
           </motion.div>
         </section>
       </article>
-
     </Layout>
   );
 }
-
--- pages/projects.js
+++ pages/projects.js
@@ -1,17 +1,17 @@
 import Layout from "../components/Layout";
-import Link from 'next/link';
+import Link from "next/link";
 import { motion } from "framer-motion";
 
 const variants = {
   hide: {
     x: -1200,
-    opacity: 0
+    opacity: 0,
   },
   show: {
     x: 0,
-    opacity: 1
-  }
-}
+    opacity: 1,
+  },
+};
 
 export default function Home() {
   return (
@@ -20,29 +20,58 @@ export default function Home() {
         <h2 className="m-16 font-bold text-4xl mb-12">Projets récents</h2>
         <section>
           <div className="flex flex-wrap justify-center w-100">
-
             <motion.div
               variants={variants}
               initial="hide"
               animate="show"
               transition={{ ease: "anticipate", duration: "0.4" }}
-              className="card relative flex m-10 w-2/5 md:w-full md:min-w-full bg-pink-600 rounded-lg transform transition-transform duration-200 ease-in-out hover:scale-105" style={{ minWidth: "800px", height: "500px", boxShadow: '0 4px 14px 0 var(--button-magenta)' }}>
-
-              <div className="w-1/2 p-16">
+              className="card relative flex m-10 w-2/5 bg-pink-600 rounded-lg transform transition-transform duration-200 ease-in-out hover:scale-105"
+              style={{
+                minWidth: "730px",
+                height: "450px",
+                boxShadow: "0 4px 14px 0 var(--button-magenta)",
+              }}
+            >
+              <div className="w-1/2 p-12 m-1">
                 <p className="text-xl text-white">2020</p>
-                <h3 className="text-5xl font-bold text-white mt-2 mb-16">Portfolio</h3>
-                <p className="text-xl text-white leading-8">Full-stack développement
+                <h3
+                  className="text-5xl font-bold text-white mt-2 mb-16"
+                  style={{ lineHeight: "3rem" }}
+                >
+                  Portfolio
+                </h3>
+                <p className="text-xl text-white leading-8">
+                  Full-stack développement
                   <br />
-                Application web
+                  Application web
                 </p>
                 <div className="absolute" style={{ bottom: "4rem" }}>
-                  <img className="inline-block w-12 mx-2" src="/images/icones_langage/nextjs.png" alt="Logo nextjs" title="Nextjs" />
-                  <img className="inline-block w-12 mx-2" src="/images/icones_langage/react.png" alt="Logo react" title="React" />
-                  <img className="inline-block w-12 mx-2" src="/images/icones_langage/tailwind.png" alt="Logo tailwindcss" title="Tailwindcss" />
+                  <img
+                    className="inline-block w-12 mx-2"
+                    src="/images/icones_langage/nextjs.png"
+                    alt="Logo nextjs"
+                    title="Nextjs"
+                  />
+                  <img
+                    className="inline-block w-12 mx-2"
+                    src="/images/icones_langage/react.png"
+                    alt="Logo react"
+                    title="React"
+                  />
+                  <img
+                    className="inline-block w-12 mx-2"
+                    src="/images/icones_langage/tailwind.png"
+                    alt="Logo tailwindcss"
+                    title="Tailwindcss"
+                  />
                 </div>
               </div>
               <div className="w-1/2 py-8">
-                <img className="block h-full rounded-l-md object-cover object-left" alt="" src="/images/images_card/portfolio.png" />
+                <img
+                  className="block w-full h-full rounded-l-md object-cover object-left"
+                  alt=""
+                  src="/images/images_card/portfolio.png"
+                />
               </div>
             </motion.div>
 
@@ -50,26 +79,57 @@ export default function Home() {
               variants={variants}
               initial="hide"
               animate="show"
-              transition={{ ease: "anticipate", duration: "0.4" }} className="card m-10 w-2/5 md:w-full md:min-w-full" style={{ minWidth: "800px", height: "500px", boxShadow: '0 4px 14px 0 var(--button-green)' }}>
-            <Link href="https://github.com/anonymze/centralisation_laravel">
-              <a target="_blank" className="flex h-full w-full bg-green-600 rounded-lg transform transition-transform duration-200 ease-in-out hover:scale-105" >
-                <div className="w-1/2 p-16">
-                  <p className="text-xl text-white">2019</p>
-                  <h3 className="text-5xl font-bold text-white mt-2 mb-16">Centralisation de stock</h3>
-                  <p className="text-xl text-white leading-8">Full-stack développement
-                  <br />
-                Application web
-                </p>
-                  <div className="absolute" style={{ bottom: "4rem" }}>
-                    <img className="inline-block w-12 mx-2" src="/images/icones_langage/laravel.png" alt="Logo Laravel" title="Laravel" />
-                    <img className="inline-block w-12 mx-2" src="/images/icones_langage/prestashop.png" alt="Logo Prestashop" title="Prestashop" />
+              transition={{ ease: "anticipate", duration: "0.4" }}
+              className="card m-10 w-2/5"
+              style={{
+                minWidth: "730px",
+                height: "450px",
+                boxShadow: "0 4px 14px 0 var(--button-green)",
+                borderRadius: "8px",
+              }}
+            >
+              <Link href="https://github.com/anonymze/centralisation_laravel">
+                <a
+                  target="_blank"
+                  className="flex h-full w-full bg-green-600 rounded-lg transform transition-transform duration-200 ease-in-out hover:scale-105"
+                >
+                  <div className="w-1/2 p-12 m-1">
+                    <p className="text-xl text-white">2019</p>
+                    <h3
+                      className="text-5xl font-bold text-white mt-2 mb-16"
+                      style={{ lineHeight: "3rem" }}
+                    >
+                      Centralisation de stock
+                    </h3>
+                    <p className="text-xl text-white leading-8">
+                      Full-stack développement
+                      <br />
+                      Application web
+                    </p>
+                    <div className="absolute" style={{ bottom: "4rem" }}>
+                      <img
+                        className="inline-block w-12 mx-2"
+                        src="/images/icones_langage/laravel.png"
+                        alt="Logo Laravel"
+                        title="Laravel"
+                      />
+                      <img
+                        className="inline-block w-12 mx-2"
+                        src="/images/icones_langage/prestashop.png"
+                        alt="Logo Prestashop"
+                        title="Prestashop"
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="w-1/2 py-8">
-                  <img className="block h-full rounded-l-md object-cover" alt="" src="/images/images_card/centralisation.png" />
-                </div>
-              </a>
-            </Link>
+                  <div className="w-1/2 py-8">
+                    <img
+                      className="block w-full h-full rounded-l-md object-cover object-left"
+                      alt=""
+                      src="/images/images_card/centralisation.png"
+                    />
+                  </div>
+                </a>
+              </Link>
             </motion.div>
           </div>
         </section>
